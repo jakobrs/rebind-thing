7229284db226280756fad9fec7517dbabed9756e
jakobrs <jakobrs100@gmail.com>
2021-05-31 16:09:04 +0200

    Add support for --prefix and --affix

--- rebind.py
+++ rebind.py
@@ -31,6 +31,8 @@ parser.add_argument('symbols', metavar='SYMBOLS', nargs='+', help='the symbols t
 
 parser.add_argument('-b', dest='binding', metavar='BIND', type=str.lower, help='change the symbol binding to BIND')
 parser.add_argument('-v', dest='visibility', metavar='VIS', type=str.lower, help='change the symbol visibility to VIS')
+parser.add_argument('--prefix', dest='prefix', metavar='PREFIX', help='Duplicate symbol and add this prefix')
+parser.add_argument('--affix', dest='affix', metavar='AFFIX', help='Duplicate symbol and add this affix')
 parser.add_argument('-o', dest='out', metavar='OUTFILE', help='set out file (default: overwrite FILE)')
 parser.add_argument('-V', dest='verbose', action='store_true', help='verbose')
 
@@ -40,12 +42,20 @@ file = lief.parse(args.file)
 
 binding = to_binding(args.binding)
 visibility = to_visibility(args.visibility)
+duplicate = args.prefix is not None or args.affix is not None
 
 for sym in args.symbols:
   if args.verbose:
     print("- Processing symbol:", sym)
 
   symbol = file.get_symbol(sym)
+
+  if duplicate:
+    symbol = file.add_static_symbol(symbol)
+    if args.prefix is not None:
+      symbol.name = args.prefix + symbol.name
+    if args.affix is not None:
+      symbol.name = symbol.name + args.affix
 
   if binding is not None and binding != symbol.binding:
     was_local = symbol.binding == lief.ELF.SYMBOL_BINDINGS.LOCAL
@@ -57,7 +67,7 @@ for sym in args.symbols:
     
     symbol.binding = binding
 
-    if was_local:
+    if was_local or duplicate:
       file.add_dynamic_symbol(symbol)
 
   if visibility is not None and visibility != symbol.visibility:
